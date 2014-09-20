659b63b9b06bbc698b6e691fec4df6f232bf5577
Zhengzhong <zhengzhongzhao@gmail.com>
2014-09-20 23:52:52 +0800

    add basic func

---
+++ app/models/group.rb
@@ -0,0 +1,4 @@
+class Group < ActiveRecord::Base
+  has_many :posts
+  validates :title, :presence => true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :groups do
+    resources :posts
+  end
+
   root to: 'visitors#index'
   devise_for :users
   resources :users

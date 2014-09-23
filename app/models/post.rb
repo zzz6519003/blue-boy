bf6ef659439bd8f837741cff290263e84bfc4894
Zhengzhong <zhengzhongzhao@gmail.com>
2014-09-23 11:15:58 +0800

    add counter cache

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :content
 
-  belongs_to :group
+  belongs_to :group, :counter_cache => true
   belongs_to :author, :class_name => "User", :foreign_key => :user_id
 
   def editable_by?(user)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140921160018) do
+ActiveRecord::Schema.define(version: 20140923031012) do
 
   create_table "group_users", force: true do |t|
     t.integer  "group_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140921160018) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "posts_count", default: 0
   end
 
   create_table "posts", force: true do |t|

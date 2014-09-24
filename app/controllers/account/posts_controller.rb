04b36f5b44f7cad3289502a85851c1f91d2b724f
Zhengzhong <zhengzhongzhao@gmail.com>
2014-09-24 12:13:17 +0800

    add post scope

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,7 +1,7 @@
 class Account::PostsController < ApplicationController
 
   def index
-    @posts = current_user.posts.order("updated_at DESC")
+    @posts = current_user.posts.recent
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
   belongs_to :group, :counter_cache => true
   belongs_to :author, :class_name => "User", :foreign_key => :user_id
 
+  scope :recent, -> { order("updated_at DESC") }
+
   def editable_by?(user)
     user && user == author
   end
